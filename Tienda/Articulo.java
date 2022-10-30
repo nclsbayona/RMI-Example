50958b4f18ab5ed5a14766f77f9ce2ef25ad1549
nclsbayona <nbayonaord@gmail.com>
2022-10-29 20:45:32 -0500

    bash and sleep

--- Tienda/Articulo.java
+++ Tienda/Articulo.java
@@ -7,23 +7,29 @@ public class Articulo implements Serializable {
     private int id;
     private String name;
     private int amount;
+
     public int getId(){
         return this.id;
     }
+    
     public int getAmount(){
         return amount;
     }
+    
     public void decrementAmount(){
         amount--;
     }
+    
     public Articulo(int id, String name, int amount){
         this.amount=amount;
         this.id=id;
         this.name=name;
     }
+    
     public String getAsCSV(){
         return id+","+name+","+amount;
     }
+    
     public String toString(){
         return "{\n \"id\": "+String.valueOf(id)+"\n \"name\": \""+name+"\"\n \"amount\": "+String.valueOf(amount)+"\n}";
     }
--- Tienda/Tienda.java
+++ Tienda/Tienda.java
@@ -21,7 +21,6 @@ public class Tienda implements InterfazMetodos {
             outstream.write(body.getBytes());
             outstream.close();
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         } 
     }
